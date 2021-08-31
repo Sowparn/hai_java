8dac61acdecb286b80dfcb5d683badac66ca0808
Sowparn <87425055+Sowparn@users.noreply.github.com>
2021-08-31 22:12:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello,baby--goodmorning");
-    System.out.println("Hello,baby--goodmorning");
-    System.out.println("Hello,baby--goodmorning");
-    System.out.println("Hello,baby--goodmorning");
-    System.out.println("Hello,baby--goodmorning");
-    System.out.println("Hello,baby--goodmorning");
-    System.out.println("Hello,baby--goodmorning");
-  }
+   }
 }
