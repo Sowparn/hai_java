1455109125550ba13b554458e08bdea741255e9a
Your Name <you@example.com>
2021-09-01 03:53:48 +0000

    added new

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello,baby--goodmorning");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-     System.out.println("Hello,baby--goodmorning");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-    System.out.println("Hello,baby--goodnight");
-   }
-}
+     }
+  }
